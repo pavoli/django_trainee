612f82092097e89eb018fb73d3084cec40d96dd0
polly <polly@Pavels-MacBook-Air.local>
2021-03-12 14:27:28 +0300

    add models to admin panel

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (
+    Author,
+    Genre,
+    Book,
+    BookInstance
+)
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register(BookInstance)
