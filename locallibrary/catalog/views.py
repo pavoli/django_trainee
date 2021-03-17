d01fbb098da45b9f8aaa1f1e78f28d277b5373a4
Pavel Olifer <pavel.olifer@gmail.com>
2021-03-17 13:13:27 +0300

    finish part7, session

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -6,9 +6,6 @@ from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
 def index(request):
-    """
-    Функция отображения для домашней страницы сайта.
-    """
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(
@@ -20,11 +17,17 @@ def index(request):
     ).count()
 
     num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
-        'num_books': num_books, 'num_instances': num_instances,
-        'num_instances_available': num_instances_available, 'num_authors': num_authors,
-        'num_genres': num_genres, 'num_books_war': num_books_war
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_war': num_books_war,
+        'num_visits': num_visits
     }
 
     return render(
